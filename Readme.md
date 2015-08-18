102303976f02c44004208b82c367f99b9e603e65
Reinaldo Colina <rcolina@pingidentity.com>
2015-08-18 14:42:55 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Hootsuite Language Distribution Monitor
+I wanted to experiment a little more with `express` and `jade` so I built this little tool to monitor recent tweets posted using `Hootsuite`, grouped by language.
+For each language, it will also tell you the count of 'verified' users that posted a tweet using `Hootsuite`.
+
+# Installation
+
++ Put your twitter app keys into keys.json
++ $ npm install
++ $ node app.js
++ point your browser to http://localhost:3000
---
+++ public/javascripts/app.js
@@ -0,0 +1,81 @@
+/*jslint browser:true */
+/*global $, Highcharts, io */
+'use strict';
+
+(function () {
+
+var clients = [],
+  languages = [],
+  newCount = [],
+  newVerifiedCount = [],
+  chart;
+
+$(document).ready(function () {
+  chart = new Highcharts.Chart({
+    chart: {
+      type: 'bar',
+      renderTo: 'container'
+    },
+    title: {
+      text: 'Hootsuite Language Distribution'
+    },
+    xAxis: {
+        categories: languages,
+        title: {
+            text: 'Languages'
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Owls (Hootsuite Users)',
+            align: 'high'
+        },
+        labels: {
+            overflow: 'justify'
+        }
+    },
+    series: [{name: 'Total Owls', data: [], color: '#71685F'}, {name: 'Famous Owls', data: [], color: '#FFBD0A'}]
+  });
+
+  var socket = io.connect();
+
+  var found;
+  socket.on('tweet', function (client) {
+    found = false;
+    for(var i in languages) {
+      if(clients[i].language === client.language) {
+        clients[i].count++;
+        if (client.isVerified) {
+          clients[i].verifiedCount++;
+        }
+        found = true;
+      }
+    }
+
+    if(!found) {
+      languages.push(client.language);
+      clients.push({language: client.language, count: 1, verifiedCount: client.isVerified && 1});
+    }
+
+  });
+});
+
+function refresh () {
+  for(var i in clients) {
+      newCount.push(clients[i].count);
+      newVerifiedCount.push(clients[i].verifiedCount);
+  }
+  chart.xAxis[0].setCategories(languages);
+  chart.series[0].setData(newCount);
+  chart.series[1].setData(newVerifiedCount);
+  chart.redraw();
+  newCount = [];
+  newVerifiedCount = [];
+
+  setTimeout(refresh, 500);
+}
+
+setTimeout(refresh, 500);
+
+})();
